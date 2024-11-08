de51668a438426b4a384992164f3ff61b3430c1d
Erika Stuart (1000106562) <erikat.stuart@gmail.com>
2024-11-09 10:19:55 +1300

    Added score UI

--- Assets/Scripts/GoalManager.cs
+++ Assets/Scripts/GoalManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GoalManager : MonoBehaviour
 {
     public static GoalManager Instance;
     private int score = 0;
     [SerializeField] private List<GameObject> goals = new List<GameObject>();
+
+    [Header("Score UI")]
+    [SerializeField] private GameObject scoreUI;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI scoreTextShadow;
+
+
 
     void Awake()
     {
@@ -18,12 +27,20 @@ public class GoalManager : MonoBehaviour
     {
         if (score == goals.Count)
         {
-            Debug.Log("Score: " + score + " / " + goals.Count);
+            Time.timeScale = 0;
+            ShowScore();
         }
     }
 
     public void AddScore()
     {
         score++;
+    }
+
+    private void ShowScore()
+    {
+        scoreUI.SetActive(true);
+        scoreText.text = score.ToString() + " / " + goals.Count;
+        scoreTextShadow.text = score.ToString() + " / " + goals.Count;
     }
 }
